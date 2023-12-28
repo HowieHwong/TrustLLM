356659738c23497681dd1a6de8eb2e43deeb1719
nauyisu022 <59754221+nauyisu022@users.noreply.github.com>
2023-12-28 17:21:33 +0800

    update

--- trustllm/task/robustness.py
+++ trustllm/task/robustness.py
@@ -65,6 +65,7 @@ class RobustnessEval:
                 else:
                     return 'Unknown'
         def check_gt(item):
+            task=item['task']
             check_res = judge(item['res'], item['task'])
 
             task_mappings = {
