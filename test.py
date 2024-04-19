d9369fae5c1e487edb297178291664152757233f
nauyisu022 <59754221+nauyisu022@users.noreply.github.com>
2024-04-19 17:12:14 +0800

    update

--- test.py
+++ test.py
@@ -10,10 +10,17 @@ from trustllm.generation.generation import LLMGeneration
 from trustllm.utils import file_process
 from trustllm import config
 
+import concurrent.futures
+
 config.replicate_api= "r8_EBJDSqZ6KDR2EmN0NWRodPva3018ceS0nd6BF"
-run_robu=LLMGeneration(test_type='fairness',
-                       online_model=True,
-                       use_replicate=True,
-              model_path='meta/meta-llama-3-70b-instruct',
-              data_path='/Users/admin/Documents/dataset')
-run_robu.generation_results()
+
+def run_task(test_type):
+    run_robu=LLMGeneration(test_type=test_type,
+                        online_model=True,
+                        use_replicate=True,
+                model_path='meta/meta-llama-3-8b-instruct',
+                data_path='/Users/admin/Documents/dataset')
+    run_robu.generation_results()
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(run_task,['safety','fairness','truthfulness','privacy','robustness','ethics'])
